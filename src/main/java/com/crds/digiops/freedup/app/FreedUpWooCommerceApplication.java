f671779ef7ad609da0d96bcc7c1e18d02d714e52
srajaiah <srajaiah@yahoo.com>
2022-09-12 15:20:03 -0400

    US21469_FreedUp_WooCommerce with git hub 2 factor authentification.

--- src/main/java/com/crds/digiops/freedup/app/FreedUpWooCommerceApplication.java
+++ src/main/java/com/crds/digiops/freedup/app/FreedUpWooCommerceApplication.java
@@ -29,28 +29,17 @@ public class FreedUpWooCommerceApplication {
 	}
 
 
-	//temporarily disables as Beth requested to run weekly citi link reports 
-	////* * 15 * * - every miniute on day 15
-	//@Scheduled(cron = "0 45 10 20 * ?")// for testing schedule a task to run at 10.45 PM 0n the 20th day of the month..
-	// at 3.00 pm daily
-	//@Scheduled(cron = "0 20 18 22 * ?")// testing on the 22  day at 6.20
-	@Scheduled(cron = "0 15 13 08 * ?")// testing on the 22  day at 6.20
-	public void scheduleTaskUsingCronExpression() throws ParseException {
+	//temporarily disabled as Beth requested to run bi-monthly freedup reports 
+	//@Scheduled(cron = "0 45 10 03 * ?")// testing on the 3  day at 10.45 AM
+	// schedule to run the first half of the previous month reports on the 17th of the month 1st to 15t 
+	@Scheduled(cron = "0 15 10 17 * ?")// schedule 17th day of the month, at 10.15 AM 
+	public void scheduleTaskUsingCronExpressionFirstHalf() throws ParseException {
 	 
 	    long now = System.currentTimeMillis() / 1000;
 	    System.out.println(
 	      "schedule tasks using cron jobs on - " + now);
 	    
-	    try {
-	    
-//		    DateFormatterUtil df = new DateFormatterUtil();
-//		    //PoliceController pc = new PoliceController();
-//		    
-//		    String startDate= df.calculateStartDateForCronJob("");    		
-//		    String endDate= df.calculateEndDateForCronJob("");
-//		    
-//		    System.out.println(" the cron start date" + startDate);
-//		    //System.out.println(" the cron end date" + endDate);
+	    try {	    
 		    WooCommerceClientStart wcs = new WooCommerceClientStart();		    
 		    String message = wcs.getWooCommerceOrders(); 		    
 		    System.out.println("**** message from scheduler" + message);		    
@@ -58,7 +47,29 @@ public class FreedUpWooCommerceApplication {
 	    }catch(Exception e) {
 	    	
 	    }
-	 }// scheduler
+	 }// scheduler first half of month
+	
+	
+	//temporarily disables as Beth requested to run weekly citi link reports 
+	//@Scheduled(cron = "0 45 10 20 * ?")// for testing schedule a task to run at 10.45 PM 0n the 20th day of the month..
+	// schedule to run on the second half of the previous month reports from 16th to the last day of teh month
+	@Scheduled(cron = "0 15 10 03 * ?")// schedule 3rd dat of Month at 10.15
+	public void scheduleTaskUsingCronExpressionSecondHalf() throws ParseException {
+	 
+	    long now = System.currentTimeMillis() / 1000;
+	    System.out.println(
+	      "schedule tasks using cron jobs on - " + now);
+	    
+	    try {
+	    
+		    WooCommerceClientStart wcs = new WooCommerceClientStart();		    
+		    String message = wcs.getWooCommerceOrders(); 		    
+		    System.out.println("**** message from scheduler" + message);		    
+	    
+	    }catch(Exception e) {
+	    	
+	    }
+	 }// scheduler second half of month
 
 } // Freed Up Application
 
